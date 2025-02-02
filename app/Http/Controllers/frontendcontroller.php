00d4629ad3368c89cd681bf67f391c6cdc13e154
Soyamstha <shrestha.soyam111@gmail.com>
2025-02-02 16:20:32 +0545

    frontend page

---
+++ app/Http/Controllers/frontendcontroller.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class frontendcontroller extends Controller
+{
+    function index()
+    {
+        return view('index');
+    }
+    function about()
+    {
+        return view('about');
+    }
+    function account()
+    {
+        return view('account');
+    }
+    function cart()
+    {
+        return view('cart');
+    }
+    function checkout()
+    {
+        return view('checkout');
+    }
+    function news_single()
+    {
+        return view('news_single');
+    }
+    function news()
+    {
+        return view('news');
+    }
+    function shop_left_sidebar()
+    {
+        return view('shop_left_sidebar');
+    }
+    function shop_single()
+    {
+        return view('shop_single');
+    }
+    function shop()
+    {
+        return view('shop');
+    }
+    function login()
+    {
+        return view('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
-
+use App\Http\Controllers\frontendcontroller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[frontendcontroller::class,'index'])->name('index');
+Route::get('/about',[frontendcontroller::class,'about'])->name('about');
+Route::get('/account',[frontendcontroller::class,'account'])->name('account');
+Route::get('/cart',[frontendcontroller::class,'cart'])->name('cart');
+Route::get('/checkout',[frontendcontroller::class,'checkout'])->name('checkout');
+Route::get('/contact',[frontendcontroller::class,'contact'])->name('contact');
+Route::get('/news_single',[frontendcontroller::class,'news_single'])->name('news_single');
+Route::get('/news',[frontendcontroller::class,'news'])->name('news');
+Route::get('/shop_left_sidebar',[frontendcontroller::class,'shop_left_sidebar'])->name('shop_left_sidebar');
+Route::get('/shop_single',[frontendcontroller::class,'shop_single'])->name('shop_single');
+Route::get('/shop',[frontendcontroller::class,'shop'])->name('shop');
+Route::get('/login',[frontendcontroller::class,'login'])->name('login');
